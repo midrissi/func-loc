5e332c5074c0a3277669d6b3ab03d64d24944847
Mohamed IDRISSI <med.idrissi@outlook.com>
2019-04-04 10:31:40 +0200

    Fix tslint errors

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,24 +1,19 @@
-declare module local {
-  interface ILocation {
-    source: string;
-    line: number;
-    column: number;
-  }
-
-  /**
-   * Locate a function
-   * @param {(...args: any[]) => void} fn The function to locate
-   * @returns {Promise<ILocation>} An object containing the source file, the line and column where the column was
-   * defined
-   */
-  function locate(fn: (...args: any[]) => any): Promise<ILocation>;
-
-  /**
-   * Release created resources
-   * @returns {Boolean} true if everything is ok, false otherwise
-   */
-  function clean(): boolean;
+export interface ILocation {
+  source: string;
+  line: number;
+  column: number;
 }
 
-export = local;
+/**
+ * Locate a function
+ * @param {(...args: any[]) => void} fn The function to locate
+ * @returns {Promise<ILocation>} An object containing the source file, the line and column where the column was
+ * defined
+ */
+export function locate(fn: (...args: any[]) => any): Promise<ILocation>;
 
+/**
+ * Release created resources
+ * @returns {Boolean} true if everything is ok, false otherwise
+ */
+export function clean(): boolean;
